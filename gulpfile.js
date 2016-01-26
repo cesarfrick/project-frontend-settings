004c922935e30ba4fcc9079a357643eeea4f81ff
Cesar Frick <cesar.frick@razorfish.com>
2016-01-26 03:16:23 -0500

    Added browsersync and created serve and dev tasks

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const source = require('vinyl-source-stream');
+const browserSync = require('browser-sync').create();
 
 /**
  * Javascript Tasks *
@@ -18,13 +19,24 @@ gulp.task('lint:js', () => {
                .pipe(linter.failAfterError());
 });
 
+gulp.task('server', () => {
+    browserSync.init({
+        server: {
+            baseDir: './app'
+        }
+    });
+
+    gulp.watch('*.html').on('change', browserSync.reload);
+});
+
 /* Development */
 gulp.task('dev:js', ['lint:js'], () => {
     return browserify('./app/src/javascript/main.js', {debug: true})
                 .transform(babelify)
                 .bundle()
                 .pipe(source('bundle.js'))
-                .pipe(gulp.dest('./app/dist/javascript/'));
+                .pipe(gulp.dest('./app/dist/javascript/'))
+                .pipe(browserSync.stream());
 });
 
 gulp.task('watch:js', ['lint:js'], () => {
@@ -43,7 +55,8 @@ gulp.task('dev:sass', () => {
                .pipe(sass.sync().on('error', sass.logError))
                .pipe(autoprefixer({browsers: ['last 2 versions', 'ie >= 9']}))
                .pipe(sourcemaps.write())
-               .pipe(gulp.dest('./app/dist/css'));
+               .pipe(gulp.dest('./app/dist/css'))
+               .pipe(browserSync.stream());
 });
 
 gulp.task('watch:sass', () => gulp.watch('./app/src/scss/**/*.scss', ['dev:sass']));
@@ -51,3 +64,5 @@ gulp.task('watch:sass', () => gulp.watch('./app/src/scss/**/*.scss', ['dev:sass'
 gulp.task('default', () => {
 
 });
+
+gulp.task('dev', ['watch:sass', 'watch:js', 'server']);
