7481db0f0b40a1e2ab1a00b8564ba7cf3e4ce0e7
Cesar Frick <cesar.frick@razorfish.com>
2016-01-25 20:23:18 -0500

    Replace Babel with Browserify and Babelify

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,22 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const linter = require('gulp-eslint');
+const browserify = require('browserify');
+const sourcemaps =  require('gulp-sourcemaps');
 
-
-gulp.task('default', () => {
-    return gulp.src('src/bundle.js')
-               .pipe(babel())
-               .pipe(gulp.dest('dist'));
-});
-
-gulp.task('lint-js', () => {
+gulp.task('lint:js', () => {
     return gulp.src(['src/javascript/**/*.js', '!node_modules/**'])
                .pipe(linter())
-               .pipe(linter.format());
+               .pipe(linter.format())
+               .pipe(linter.failAfterError());
+});
+
+gulp.task('babel', ['lint:js'], () => {
+    return gulp.src(['src/javascript/**/*.js', '!node_modules/**'])
+               .pipe(babel())
+               .pipe(gulp.dest('dist/javascript'));
+});
+
+gulp.task('default', () => {
+
 });
